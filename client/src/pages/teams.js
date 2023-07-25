760a9db20fce49f6ee14ba58224534a88dd753cb
J Neal <jasmineneal101@gmail.com>
2023-07-25 21:54:11 +1000

    push code

--- client/src/pages/teams.js
+++ client/src/pages/teams.js
@@ -4,13 +4,20 @@ import { useQuery } from '@apollo/client';
 
 // import ThoughtList from '../components/ThoughtList';
 import TeamPostForm from '../components/teamPostForm';
+import { useEffect } from 'react';
 
 import { QUERY_THOUGHTBYTEAM } from '../utils/queries';
 
 const Team = () => {
   const { teamId } = useParams();
 const { loading, data } = useQuery(QUERY_THOUGHTBYTEAM, {variables: {teamId}});
-const teamPosts = data?.thoughts || [];
+const teamPosts = data?.postbyteam || [];
+
+useEffect(() => {
+  console.log('data from query');
+  console.log(data);
+}, [loading, data])
+
 
 console.log("data",teamPosts);
 
@@ -33,6 +40,17 @@ console.log("data",teamPosts);
               title="Some Feed for Thought(s)..."
             />
           )} */}
+
+<h4>Viewing posts for team {"...add team name...."}</h4>
+          <h5>Team Posts:</h5>
+          {teamPosts.map((post) => (
+            <div key={post.id}>
+              <h6>{post.title}</h6>
+              <p>{post.description}</p>
+              <hr />
+            </div>
+          ))}
+
           Viewing details for team {teamId}
         </div>
       </div>

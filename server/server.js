c3606273bcece6611d19daf3e30c2c04bde03d50
J Neal <jasmineneal101@gmail.com>
2023-07-27 00:25:43 +1000

    push update

--- server/server.js
+++ server/server.js
@@ -20,11 +20,17 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Project...' })
+  })
+}
 
 
 // Create a new instance of an Apollo server with the GraphQL schema
